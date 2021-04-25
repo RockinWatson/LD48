348b4e8001e807762ab6ac28440b07b9f4ba8683
Greg Raab <greg@recroom.com>
2021-04-25 01:18:27 -0700

    Basic Dash behavior

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -4,6 +4,7 @@ using UnityEngine.Events;
 public class CharacterController2D : MonoBehaviour
 {
 	[SerializeField] private float m_JumpForce = 400f;                          // Amount of force added when the player jumps.
+	[SerializeField] private float m_DashForce = 1600f;                         // Amount of force added when the player dashes.
 	[Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;          // Amount of maxSpeed applied to crouching movement. 1 = 100%
 	[Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;  // How much to smooth out the movement
 	[SerializeField] private bool m_AirControl = false;                         // Whether or not a player can steer while jumping;
@@ -79,7 +80,6 @@ public class CharacterController2D : MonoBehaviour
 		//only control the player if grounded or airControl is turned on
 		if (m_Grounded || m_AirControl)
 		{
-
 			// If crouching
 			if (crouch)
 			{
@@ -152,5 +152,12 @@ public class CharacterController2D : MonoBehaviour
 		Vector3 theScale = transform.localScale;
 		theScale.x *= -1;
 		transform.localScale = theScale;
+	}
+
+	public void Dash()
+    {
+		var dashForce = new Vector2(m_DashForce, 0f);
+		dashForce *= (m_FacingRight ? 1f : -1f);
+		m_Rigidbody2D.AddForce(dashForce);
 	}
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,6 +7,13 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Animator _animator = null;
     [SerializeField] private float _movementSpeed = 1f;
 
+    [SerializeField]
+    private float _dashTimeThreshold = 0.5f;
+    private float _dashTimer = 0.0f;
+    private bool _possibleDash = false;
+    private float _possibleDashDirection = 0f;
+    private bool _dash = false;
+
     private CharacterController2D _characterController2D = null;
 
     // State variables:
@@ -34,6 +41,8 @@ public class PlayerMovement : MonoBehaviour
     {
         _horizontalMove = Input.GetAxisRaw("Horizontal") * _movementSpeed;
 
+        UpdateDashMovement();
+
         //var doJump = false;
         if (Input.GetKeyDown(KeyCode.Space))
         {
@@ -56,6 +65,37 @@ public class PlayerMovement : MonoBehaviour
         //}
     }
 
+    private void UpdateDashMovement()
+    {
+        if (_possibleDash)
+        {
+            _dashTimer += Time.deltaTime;
+            if (_dashTimer >= _dashTimeThreshold)
+            {
+                _possibleDash = false;
+            }
+        }
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            _dashTimer = 0f;
+            if (_possibleDash)
+            {
+                if (Mathf.Approximately(_horizontalMove, _possibleDashDirection))
+                {
+                    // Let's dash!
+                    _dash = true;
+
+                    _possibleDash = false;
+                }
+            }
+            else
+            {
+                _possibleDash = true;
+                _possibleDashDirection = _horizontalMove;
+            }
+        }
+    }
+
     public void OnLanding()
     {
         _animator?.SetBool("IsJumping", false);
@@ -68,6 +108,12 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if(_dash)
+        {
+            _characterController2D.Dash();
+            _dash = false;
+        }
+
         _characterController2D.Move(_horizontalMove, _crouch, _jump);
         _jump = false;
     }
