3f13c98450abd8c370379a6d2e9c131057b135d7
Greg Raab <greg@recroom.com>
2021-04-26 18:39:02 -0700

    Game Win hook up

--- Assets/Scripts/GlobalManager.cs
+++ Assets/Scripts/GlobalManager.cs
@@ -7,7 +7,9 @@ public class GlobalManager : MonoBehaviour
     private static GlobalManager _instance = null;
     public static GlobalManager Get() { return _instance; }
 
-    private float _playerHealthPercent = 25F;
+    private float _pizzaHealthPercent = 25F;
+    private float _pizzaDepth = 25F;
+    private float _pizzaHeat = 25F;
 
     private void Awake()
     {
@@ -21,8 +23,25 @@ public class GlobalManager : MonoBehaviour
         GameObject.DontDestroyOnLoad(_instance.gameObject);
     }
 
-    public float GetPlayerHealthPercent()
+    public float GetPizzaHealthPercent()
     {
-        return _playerHealthPercent;
+        return _pizzaHealthPercent;
+    }
+
+    public float GetPizzaDepth()
+    {
+        return _pizzaHealthPercent;
+    }
+
+    public float GetPizzaHeat()
+    {
+        return _pizzaHeat;
+    }
+
+    public void SetGameWinStats(float pizzaHealthPercent, float pizzaDepth, float pizzaHeat)
+    {
+        _pizzaHealthPercent = pizzaHealthPercent;
+        _pizzaDepth = pizzaDepth;
+        _pizzaHeat = pizzaHeat;
     }
 }
